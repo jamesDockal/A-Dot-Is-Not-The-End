65b0ce12f94de95b906c73b5bce0632816c074ec
james dockal <jamesistranho@gmail.com>
2021-07-29 01:02:36 -0300

    feat: create Register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Register from "./pages/Register";
 import "./styles/app.css";
 
 function App() {
-  return <div className="App">teste</div>;
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Psychometrika</h1>
+      <h3>Desafio Trainee</h3>
+
+      <input type="text" />
+      <label>Email</label>
+
+      <input type="text" />
+      <label>Senha</label>
+    </div>
+  );
+}
